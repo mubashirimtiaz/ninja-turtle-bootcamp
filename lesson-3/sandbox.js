186aa2b7f0a47c1704deb6288ceb7d6d656bbd72
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2020-09-07 23:55:15 +0500

    Added lesson-3 source code

---
+++ lesson-3/sandbox.js
@@ -0,0 +1,136 @@
+//number
+
+let age = 45;
+let decimal = 3.43;
+
+//maths operations
+
+// +,-,*,/,**,%
+
+// let radius = 35;
+// const pi = 3.142;
+
+//power
+
+// let radiusSqr = radius ** 2;
+
+// console.log(radiusSqr);
+
+// let area = pi * radiusSqr;
+
+// console.log(area);
+
+//modulas
+const rmd = 20 % 3;
+
+// console.log(rmd);
+
+//division
+
+const div = 30 / 3;
+
+// console.log(div);
+
+//multiplication
+
+// const mul = 3 * 5;
+
+// console.log(mul);
+
+//BIDMAS Rule
+
+// const bidmas = 10 * (2 * 3) ** 3;
+
+// console.log(bidmas);
+
+// let numOne = 10;
+
+// numOne = numOne + 10;
+
+// numOne+=10;
+
+// numOne *= 2;
+
+// numOne /= 2;
+
+// numOne -= 6;
+
+// console.log(numOne);
+
+// let _string = "hello";
+
+// let num = 4;
+
+// console.log(_string + num);
+
+// let book = "A Dark Knight";
+// let author = "Mudassir";
+// let likes = 45;
+
+// let longSting =
+//   "A book called " +
+//   book +
+//   " written by " +
+//   author +
+//   " has " +
+//   likes +
+//   " likes";
+
+// console.log(longSting);
+
+//template string or string literal
+
+// let longString = `A book named ${book} written by ${author} has ${likes} likes.`;
+// console.log(longString);
+
+// let html = `<h1>Hello World </h1>`;
+
+// console.log(html);
+
+// Array
+
+let friends = ["Mubashir", "Mudassir", "Faaiz", "Hammad"];
+
+console.log(friends);
+
+let mix = ["John", 23, { name: "Shaun" }];
+
+// console.log(mix);
+
+//length property
+
+// console.log(friends.length);
+
+// getting element
+
+// let indOne = friends[2];
+
+// console.log(indOne);
+
+// Array Methods
+
+//join
+
+// let _str = friends.join(",");
+
+// let _str = friends.join(",");
+
+// console.log(_str, typeof _str);
+
+// indexOf
+
+let getIndex = friends.indexOf("faaiz");
+
+console.log(getIndex);
+
+// concate
+
+let getMixture = mix.concat(friends);
+[friends, mix];
+console.log(getMixture);
+
+//push
+
+let newArray = friends.push("Aamir");
+
+console.log(newArray, friends);
