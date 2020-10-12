9508ed82533cd666173e548fdc92e8bbdf05332a
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2020-10-12 23:52:44 +0500

    added quiz app

---
+++ quiz_app/quiz.js
@@ -0,0 +1,21 @@
+//list of correct answers
+const correctAnswers = ["B","A","B","B","A"];
+
+// quering  DOM
+const form = document.querySelector('.quiz-form');
+const result  = document.querySelector('.result')
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const selectAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value]
+    let score= 0
+    selectAnswers.forEach((ans,index)=>{
+        if(ans === correctAnswers[index]){
+            score++;
+        }
+    })
+    score  = (score / selectAnswers.length) *100;
+    result.querySelector('span').innerText =`${score}%`
+    result.classList.remove('d-none');
+    scroll(0,0);
+})
